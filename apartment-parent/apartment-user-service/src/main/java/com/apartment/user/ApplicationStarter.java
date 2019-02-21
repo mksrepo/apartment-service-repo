5838efd06f8d9638a6a7d8c8bf8bdd5e388bedfe
264030 <264030@PC399852.cts.com>
2019-02-21 12:57:20 +0530

    Committed initial version

--- apartment-parent/apartment-user-service/src/main/java/com/apartment/user/ApplicationStarter.java
+++ apartment-parent/apartment-user-service/src/main/java/com/apartment/user/ApplicationStarter.java
@@ -3,8 +3,10 @@ package com.apartment.user;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 @EnableEurekaClient
+@EnableResourceServer
 @SpringBootApplication
 public class ApplicationStarter {
 
